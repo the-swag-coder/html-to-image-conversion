3ee7d3c6d9eb9f8398e3a8d35447a475bf538a0e
Swagat Suman <swagat@sulopa.in>
2020-06-10 12:14:20 +0530

    HTML to image conversion and download image functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import htmlToImage from 'html-to-image';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'html-to-image-conversion';
+
+  public contentList = [
+    {
+      background: '#3B5998',
+      icon: 'fa fa-facebook-square',
+      filename: 'facebook'
+    },
+    {
+      background: '#BB0000',
+      icon: 'fa fa-youtube-square',
+      filename: 'youtube'
+    },
+    {
+      background: '#55ACEE',
+      icon: 'fa fa-twitter-square',
+      filename: 'twitter'
+    }
+  ];
+
+  downloadAsPng(filename: string, index: number): void {
+    document.getElementById(`non-printable-${index}`).style.display = 'none';
+
+    htmlToImage.toPng(document.getElementById(`printable-${index}`)).then((dataurl) => {
+      const link: any = document.createElement('a');
+
+      link.download = `${filename}.png`;
+      link.href = dataurl;
+      link.click();
+
+      document.getElementById(`non-printable-${index}`).style.display = 'block';
+    });
+  }
+
 }
